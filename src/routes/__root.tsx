f9af8233afc8887c1889788e341c178c76db3040
Felix Avalos <felavalosg@gmail.com>
2025-10-26 13:56:59 -0600

    Remove auth access link

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,12 @@
-import { useEffect } from 'react';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 const RootLayout = () => {
-  useEffect(() => {
-    console.log('Navigated');
-  }, []);
-
   return (
     <>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Inicio
-        </Link>
-        <Link to="/auth" className="[&.active]:font-bold">
-          Auth
         </Link>
       </div>
 
