7f0fe8d30086de311681660e602847d60ca8062c
Felix Avalos <felavalosg@gmail.com>
2025-11-05 20:56:22 -0600

    Home page responsive design and styles

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,17 +7,19 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="w-full p-2">
-      <h2>Torneos</h2>
-      <Button
-        title='Crear torneo'
-        action={() => console.log('Crear torneo')}
-      />
-
-      <div>
-        <TournamentCard
-
+    <div className="w-full p-3 lg:max-w-[970px] lg:justify-self-center">
+      <div className="flex flex-col text-center gap-1.5 md:flex-row md:justify-between md:items-center">
+        <h2 className="text-xl md:text-2xl font-semibold">Mis Torneos</h2>
+        <Button
+          title='Crear torneo'
+          action={() => console.log('Crear torneo')}
         />
+      </div>
+
+      <div className="flex flex-col items-center mt-5 gap-5 md:flex-row md:flex-wrap md:justify-center">
+        <TournamentCard />
+        <TournamentCard />
+        <TournamentCard />
       </div>
     </div>
   );
